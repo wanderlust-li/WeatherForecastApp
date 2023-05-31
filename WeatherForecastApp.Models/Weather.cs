c2a3efeec6a42aa0ed0b3cf08069137b49fcdd1b
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-31 19:53:26 +0300

    weather model

---
+++ WeatherForecastApp.Models/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherForecastApp.Models;
+
+public class Weather
+{
+    [Key]
+    public int Id { get; set; }
+    public string City { get; set; }
+    public float Temperature { get; set; }
+    public float Humidity { get; set; } // вологість
+    public float WindSpeed { get; set; } // швидкість вітру
+    public string WeatherDescription { get; set; } // опис погоди
+    public DateTime DateTime { get; set; } = DateTime.Now;
+}
