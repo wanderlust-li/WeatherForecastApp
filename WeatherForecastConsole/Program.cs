b1cc0e700e3585745af9715b8148961444597de6
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-01 12:07:14 +0300

    update console app

--- WeatherForecastConsole/Program.cs
+++ WeatherForecastConsole/Program.cs
@@ -2,43 +2,47 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 
-class Program
+namespace WeatherForecastConsole
 {
-    static async Task Main(string[] args)
+    class Program
     {
-        Console.Write("Enter a city: ");
-        string? city = Console.ReadLine();
-
-        try
+        static async Task Main()
         {
-            string apiKey = ""; // Your OpenWeatherMap API key
+            Console.Write("Enter a city: ");
+            string? city = Console.ReadLine();
 
-            using (HttpClient client = new HttpClient())
+            try
             {
-                string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
-                HttpResponseMessage response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-                string responseBody = await response.Content.ReadAsStringAsync();
-                
-                dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
-                
-                string temperature = data.main.temp;
-                string humidity = data.main.humidity;
-                string windSpeed = data.wind.speed;
-                string weatherDescription = data.weather[0].description;
-                DateTime dateTime = DateTime.Now;
-                
-                Console.WriteLine($"City: {city}");
-                Console.WriteLine($"Temperature: {temperature}°C");
-                Console.WriteLine($"Humidity: {humidity}%");
-                Console.WriteLine($"Wind Speed: {windSpeed} m/s");
-                Console.WriteLine($"Weather Description: {weatherDescription}");
-                Console.WriteLine($"Date and Time: {dateTime}");
+                string apiKey = ""; // Your OpenWeatherMap API key
+
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody) ??
+                                   throw new InvalidOperationException();
+
+                    string temperature = data.main.temp;
+                    string humidity = data.main.humidity;
+                    string windSpeed = data.wind.speed;
+                    string weatherDescription = data.weather[0].description;
+                    DateTime dateTime = DateTime.Now;
+
+                    Console.WriteLine($"City: {city}");
+                    Console.WriteLine($"Temperature: {temperature}°C");
+                    Console.WriteLine($"Humidity: {humidity}%");
+                    Console.WriteLine($"Wind Speed: {windSpeed} m/s");
+                    Console.WriteLine($"Weather Description: {weatherDescription}");
+                    Console.WriteLine($"Date and Time: {dateTime}");
+                }
             }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
     }
 }
