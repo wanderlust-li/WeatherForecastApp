cf7f8d1145de29cf6360659e61dae9985daaf314
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-01 12:14:07 +0300

    update context db

---
+++ WeatherForecastApp.Data/WeatherDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherForecastApp.Models;
+
+namespace WeatherForecastApp.Data;
+
+public class WeatherDbContext : DbContext
+{
+    public WeatherDbContext()
+    {
+    }
+
+    public WeatherDbContext(DbContextOptions<WeatherDbContext> context) : base(context)
+    {
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Weather;Trusted_Connection=True;");
+    }
+    
+    public DbSet<Weather> Weathers { get; set; }
+}
--- WeatherForecastApp.Models/Weather.cs
+++ WeatherForecastApp.Models/Weather.cs
@@ -6,10 +6,12 @@ public class Weather
 {
     [Key]
     public int Id { get; set; }
+    
     public string City { get; set; }
     public float Temperature { get; set; }
     public float Humidity { get; set; } // вологість
     public float WindSpeed { get; set; } // швидкість вітру
     public string WeatherDescription { get; set; } // опис погоди
+    
     public DateTime DateTime { get; set; } = DateTime.Now;
 }
