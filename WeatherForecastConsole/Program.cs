3f02d4036ae2668f9ff721c4937cb3ca5b649614
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-01 12:00:39 +0300

    console application

---
+++ WeatherForecastConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Write("Enter a city: ");
+        string? city = Console.ReadLine();
+
+        try
+        {
+            string apiKey = ""; // Your OpenWeatherMap API key
+
+            using (HttpClient client = new HttpClient())
+            {
+                string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                
+                dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+                
+                string temperature = data.main.temp;
+                string humidity = data.main.humidity;
+                string windSpeed = data.wind.speed;
+                string weatherDescription = data.weather[0].description;
+                DateTime dateTime = DateTime.Now;
+                
+                Console.WriteLine($"City: {city}");
+                Console.WriteLine($"Temperature: {temperature}°C");
+                Console.WriteLine($"Humidity: {humidity}%");
+                Console.WriteLine($"Wind Speed: {windSpeed} m/s");
+                Console.WriteLine($"Weather Description: {weatherDescription}");
+                Console.WriteLine($"Date and Time: {dateTime}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
