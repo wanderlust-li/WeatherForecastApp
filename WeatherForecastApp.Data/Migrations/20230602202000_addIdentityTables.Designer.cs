df1ed60e414f18581b4e2a3fd7290f10f75c6449
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-02 23:21:05 +0300

    add identity tables

--- WeatherForecastApp.Data/Migrations/20230602202000_addIdentityTables.Designer.cs
+++ WeatherForecastApp.Data/Migrations/20230602202000_addIdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WeatherForecastApp.Data;
 
@@ -11,9 +12,11 @@ using WeatherForecastApp.Data;
 namespace WeatherForecastApp.Data.Migrations
 {
     [DbContext(typeof(WeatherDbContext))]
-    partial class WeatherDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230602202000_addIdentityTables")]
+    partial class addIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
