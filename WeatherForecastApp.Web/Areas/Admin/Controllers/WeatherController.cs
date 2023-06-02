11d8d4accc56dd8f5d76e9b2dd75008a92ef79a4
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-02 23:28:47 +0300

    add weather controller

---
+++ WeatherForecastApp.Web/Areas/Admin/Controllers/WeatherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecastApp.Web.Areas.Admin.Controllers;
+[Area("Admin")]
+public class WeatherController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
