aa41dddd035c4e74aff00ce289167c1f5578282a
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-02 21:48:06 +0300

    bootswatch lux

--- WeatherForecastApp.Web/Controllers/HomeController.cs
+++ WeatherForecastApp.Web/Controllers/HomeController.cs
@@ -1,31 +1,36 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WeatherForecastApp.Data;
+using WeatherForecastApp.Models;
 using WeatherForecastApp.Web.Models;
 
-namespace WeatherForecastApp.Web.Controllers;
-
-public class HomeController : Controller
+namespace WeatherForecastApp.Web.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly WeatherDbContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, WeatherDbContext context)
+        {
+            _context = context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
