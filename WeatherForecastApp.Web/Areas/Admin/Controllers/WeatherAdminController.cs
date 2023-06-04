69b9e563a55cd9d4350e238a795f3bce77397e70
wanderlust-li <zhekalichm4n@icloud.com>
2023-06-04 14:28:28 +0300

    delete page

--- WeatherForecastApp.Web/Areas/Admin/Controllers/WeatherAdminController.cs
+++ WeatherForecastApp.Web/Areas/Admin/Controllers/WeatherAdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using WeatherForecastApp.Data;
 using WeatherForecastApp.Models;
@@ -22,4 +23,33 @@ public class WeatherAdminController : Controller
 
         return View(weathers);
     }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == 0 || id == null)
+            return NotFound();
+        var weather = _context.Weathers.FirstOrDefault(u => u.Id == id);
+        if (weather == null)
+            return NotFound();
+
+        return View(weather);
+    }
+    
+    // POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int ?id)
+    {
+        var weather = _context.Weathers.FirstOrDefault(u => u.Id == id);
+
+        if (weather == null)
+            return NotFound();
+
+        _context.Weathers.Remove(weather);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
+    
+    
 }
